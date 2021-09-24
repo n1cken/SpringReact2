d3f19fc48cba5420de703cd874edf49f71668f22
nicholas <nicken333@gmail.com>
2021-09-24 10:36:03 +0200

    Fullstack working

---
+++ frontend/react-app/src/components/UserComponent.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import UserService from '../services/UserService';
+
+class UserComponent extends React.Component{
+
+   constructor(props) {
+       super(props);
+
+        /* Create so that the state of this component has a
+         empty users array. */
+
+       this.state = {
+           users:[]
+       }
+   }
+
+    /*Called immediately after mounted, calling setState here
+    will trigger to re-render. */
+
+   componentDidMount() {
+       UserService.getUsers().then((response) => {
+           this.setState({users: response.data})
+       });
+
+   }
+
+    render () {
+        return (
+            <div>
+                <h1 className="text text-center"> User List </h1>
+                <table className="table table-striped">
+                    <thead>
+                        <tr>
+                            <td>User Id</td>
+                            <td>First Name</td>
+                            <td>Last Name</td>
+                            <td>Mail</td>
+
+                        </tr>
+                    </thead>
+                <tbody>
+                {
+                    this.state.users.map(
+                        /* map is called for each object
+                            in an array.
+
+                        function user() {
+
+                         } */
+
+                        user =>
+                            <tr key={user.id}>
+                                <td> {user.id}</td>
+                                <td> {user.firstName}</td>
+                                <td> {user.lastName}</td>
+                                <td> {user.email}</td>
+                            </tr>
+                    )
+                }
+                </tbody>
+                </table>
+            </div>
+        )
+    }
+}
+
+export default UserComponent;
